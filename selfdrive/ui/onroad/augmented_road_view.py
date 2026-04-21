43abf8b2e6d659cdff7e5f30c40e4813f5fbdbf5
zhangxu <xu.zhang@dlssa.com>
2026-04-21 19:48:12 +0800

    2026.04.21 修复缩进问题

--- selfdrive/ui/onroad/augmented_road_view.py
+++ selfdrive/ui/onroad/augmented_road_view.py
@@ -85,11 +85,11 @@ class AugmentedRoadView(CameraView):
 
     # # Only render UI overlays when system is started
     # if ui_state.started:
-      # Draw all UI overlays
-      self.model_renderer.render(self._content_rect)
-      self._hud_renderer.render(self._content_rect)
-      self.alert_renderer.render(self._content_rect)
-      self.driver_state_renderer.render(self._content_rect)
+    # Draw all UI overlays
+    self.model_renderer.render(self._content_rect)
+    self._hud_renderer.render(self._content_rect)
+    self.alert_renderer.render(self._content_rect)
+    self.driver_state_renderer.render(self._content_rect)
 
     # Custom UI extension point - add custom overlays here
     # Use self._content_rect for positioning within camera bounds
